8ea06cdeda41024860aa1256769ba9b9849010e9
JUSTIN JOUBERT <PROTOSS.JUSTIN@GMAIL.COM>
2016-03-16 16:30:46 +0400

    Background and button adding for Opening Screen Login Activity

---
+++ app/src/main/java/com/csci342/justin/moodleapplication/Dashboard.java
@@ -0,0 +1,13 @@
+package com.csci342.justin.moodleapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dashboard extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
