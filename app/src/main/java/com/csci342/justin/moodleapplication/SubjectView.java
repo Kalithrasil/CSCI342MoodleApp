323a47a8e39ced860bba03d13e5736ddfa1a7ceb
JUSTIN JOUBERT <PROTOSS.JUSTIN@GMAIL.COM>
2016-03-18 23:04:34 +0400

    Created the Subject VIew (Teacher) layout

--- app/src/main/java/com/csci342/justin/moodleapplication/SubjectView.java
+++ app/src/main/java/com/csci342/justin/moodleapplication/SubjectView.java
@@ -11,6 +11,7 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import layout.UploadAssignment;
+import layout.ViewEnrolledStudents;
 import layout.ViewGrades;
 import layout.ViewResources;
 
@@ -26,26 +27,33 @@ public class SubjectView extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_subject_view);
+
 
         previous = getIntent();
         authority = previous.getStringExtra("Authority");
         String subject = previous.getStringExtra("Subject Name");
 
-        TextView temp = (TextView) findViewById(R.id.SV_subjectname_textview);
-        temp.setText(subject);
+
 
         if(authority.equals("Teacher"))
         {
+            setContentView(R.layout.activity_subject_view_teacher);
+            TextView temp = (TextView) findViewById(R.id.SVT_subjectname_textview);
+            temp.setText(subject);
+
             frag = new ViewResources();
             FragmentTransaction ft = fm.beginTransaction();
 
-            tabs = (FrameLayout) findViewById(R.id.SV_tabview_framelayout);
+            tabs = (FrameLayout) findViewById(R.id.SVT_tabsview_framelayout);
             tabs.removeAllViews();
-            ft.replace(R.id.SV_tabview_framelayout, frag).commit();
+            ft.replace(R.id.SVT_tabsview_framelayout, frag).commit();
         }
         else
         {
+            setContentView(R.layout.activity_subject_view);
+            TextView temp = (TextView) findViewById(R.id.SV_subjectname_textview);
+            temp.setText(subject);
+
             frag = new ViewResources();
             FragmentTransaction ft = fm.beginTransaction();
 
@@ -63,6 +71,14 @@ public class SubjectView extends Activity {
         ft.replace(R.id.SV_tabview_framelayout, frag).commit();
     }
 
+    public void switchToViewResourcesTeacher(View v)
+    {
+        frag = new ViewResources();
+        FragmentTransaction ft = fm.beginTransaction();
+        tabs.removeAllViews();
+        ft.replace(R.id.SVT_tabsview_framelayout, frag).commit();
+    }
+
     public void switchToViewGrades(View v)
     {
         frag = new ViewGrades();
@@ -77,5 +93,21 @@ public class SubjectView extends Activity {
         FragmentTransaction ft = fm.beginTransaction();
         tabs.removeAllViews();
         ft.replace(R.id.SV_tabview_framelayout, frag).commit();
+    }
+
+    public void switchToUploadResources(View v)
+    {
+        frag = new UploadResources();
+        FragmentTransaction ft = fm.beginTransaction();
+        tabs.removeAllViews();
+        ft.replace(R.id.SVT_tabsview_framelayout, frag).commit();
+    }
+
+    public void switchToViewEnrolledStudents(View v)
+    {
+        frag = new ViewEnrolledStudents();
+        FragmentTransaction ft = fm.beginTransaction();
+        tabs.removeAllViews();
+        ft.replace(R.id.SVT_tabsview_framelayout, frag).commit();
     }
 }
