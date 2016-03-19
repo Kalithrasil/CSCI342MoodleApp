537ba363497e70df3b5afd85c51445ffd62d7c8b
JUSTIN JOUBERT <PROTOSS.JUSTIN@GMAIL.COM>
2016-03-19 13:03:45 +0400

    Changed View University Details stuff

--- app/src/main/java/com/csci342/justin/moodleapplication/ViewUniversityDetails.java
+++ app/src/main/java/com/csci342/justin/moodleapplication/ViewUniversityDetails.java
@@ -7,9 +7,12 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
+import layout.ContactUs;
 import layout.ViewCalendar;
+import layout.ViewPolicies;
 
 public class ViewUniversityDetails extends Activity {
 
@@ -35,23 +38,40 @@ public class ViewUniversityDetails extends Activity {
         tabs.removeAllViews();
         ft.replace(R.id.VUD_tabsview_framelayout, frag).commit();
 
+        Button temp = (Button) findViewById(R.id.VUD_AddEve_button);
+
+        if(authority.equals("Teacher"))
+        {
+            temp.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            temp.setVisibility(View.INVISIBLE);
+        }
+
     }
 
     public void switchToViewCalendar(View v)
     {
-        frag = new EditPersonalDetails();
+        frag = new ViewCalendar();
         FragmentTransaction ft = fm.beginTransaction();
         tabs.removeAllViews();
-        ft.replace(R.id.D_tabview_framelayout, frag).commit();
+        ft.replace(R.id.VUD_tabsview_framelayout, frag).commit();
     }
 
     public void switchToViewContactUs(View v)
     {
-
+        frag = new ContactUs();
+        FragmentTransaction ft = fm.beginTransaction();
+        tabs.removeAllViews();
+        ft.replace(R.id.VUD_tabsview_framelayout, frag).commit();
     }
 
     public void switchToViewPolicies(View v)
     {
-
+        frag = new ViewPolicies();
+        FragmentTransaction ft = fm.beginTransaction();
+        tabs.removeAllViews();
+        ft.replace(R.id.VUD_tabsview_framelayout, frag).commit();
     }
 }
